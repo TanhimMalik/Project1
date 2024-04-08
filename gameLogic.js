04ed4c373c767058fb7e47f632d7c79acc1a9a52
Tanhim Malik <88328356+TanhimMalik@users.noreply.github.com>
2024-04-08 12:57:17 -0400

    Add final project files

---
+++ gameLogic.js
@@ -0,0 +1,21 @@
+const choices = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return "It's a tie!";
+    }
+    if ((userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'scissors' && computerChoice === 'paper') ||
+        (userChoice === 'paper' && computerChoice === 'rock')) {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
+}
+
+module.exports = { determineWinner, getComputerChoice, choices };
